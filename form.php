065658c8abecd5889c1b83a9a40f5c9ce9848b3c
Rohas Nagpal <rohasnagpal@gmail.com>
2020-01-19 11:11:55 +0530

    Added form

---
+++ form.php
@@ -0,0 +1,17 @@
+            <form class="currency-form" action="demo_process.php" method="post">
+              <h4>Request a demo</h4>
+              <div class="form-field">
+                <label>Name</label>
+                <div class="join-field">
+                  <input type="text" name="name" placeholder="" required>
+                </div>
+              </div>
+              <div class="form-field">
+                <label>Email</label>
+                <div class="join-field">
+                  <input type="email" name="email" placeholder="" required="required">
+                </div>
+              </div>
+              <button class="btn btn-block btn-filled form-btn"> <span class="bh"></span> <span>Request a demo</span><i class="fas fa-arrow-right"></i></button>
+              <span class="accept-terms">Don't need a demo yet? <a href="https://calendly.com/primechain/10min" target="_blank">Book a 10 min discussion.</a></span>
+            </form>
